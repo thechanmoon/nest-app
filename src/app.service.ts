6241a51dd60bb5b10a44d2c33960528b07c800ae
chanp ark <thechanmoon@gmail.com>
2020-09-29 15:09:28 -0400

    Hello World

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+	getHello(): string {
+		return 'Hello Nest World!'
+	}
+	getHi(): string {
+		return 'Hi Nest World!'
+	}
 }
